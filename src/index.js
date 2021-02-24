707f33a591df563f6e50df95258f32cbe4b09d15
Amychen <sailor_mi@163.com>
2021-02-24 23:49:04 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./index.css";
 import { root } from "@router";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 
 const routeKey = [];
-function sendToAnalytics(metric) {
-  // console.log(metric);
-  // const body = JSON.stringify(metric);
-  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-  // (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
-  //     fetch('/analytics', {body, method: 'POST', keepalive: true});
-}
+// function sendToAnalytics(metric) {
+//   // console.log(metric);
+//   // const body = JSON.stringify(metric);
+//   // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+//   // (navigator.sendBeacon && navigator.sendBeacon('/analytics', body)) ||
+//   //     fetch('/analytics', {body, method: 'POST', keepalive: true});
+// }
 const supportsHistory = "pushState" in window.history;
 
 ReactDOM.render(
@@ -40,4 +40,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(sendToAnalytics);
+// reportWebVitals(sendToAnalytics);
